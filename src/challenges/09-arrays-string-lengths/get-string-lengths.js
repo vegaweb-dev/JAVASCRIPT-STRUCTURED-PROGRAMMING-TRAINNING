32360052c9f2274e087218a4d8a55cbf740cab51
cvega <carlosjuniorrvega@hotmail.com>
2025-11-12 17:52:52 -0500

    feat(09-arrays-string-lengths): solve challenge get-string-lengths.js. CV

---
+++ src/challenges/09-arrays-string-lengths/get-string-lengths.js
@@ -0,0 +1,1 @@
+export const getStringLengths=(arr)=>arr.map(el=>el.length);
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { removeDuplicates } from "./challenges/05-Arrays-remove-duplicates/remov
 import { sumEvenNumbers } from "./challenges/06-arrays-sum-even-numbers/sum-even-numbers.js";
 import { getOddNumbers } from "./challenges/07-arrays-odd-numbers/odd-numbers.js";
 import { multiplyAll } from "./challenges/08-arrays-multiply-elements/multiply-elements.js";
+import { getStringLengths } from "./challenges/09-arrays-string-lengths/get-string-lengths.js";
 console.log(containElement([3,3,4,5,6],6));
 console.log(findLargestNumber([1,3,10,5,6,7]))
 console.log(countOccurrences([1,2,2,3,2],2))
@@ -16,3 +17,4 @@ console.log(removeDuplicates([1,2,2,3,3,4]))
 console.log(sumEvenNumbers([2,2,3]))
 console.log(getOddNumbers([1, 2, 3, 4, 5, 6]))
 console.log(multiplyAll([1,2,3],3))
+console.log(getStringLengths(["cat","house","sun"]))
