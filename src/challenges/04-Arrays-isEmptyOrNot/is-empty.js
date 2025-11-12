f92af45377283fea183e1f7536f7050d38415591
cvega <carlosjuniorrvega@hotmail.com>
2025-11-12 15:42:16 -0500

    feat(04-Arrays-isEmptyOrNot): solve isEmpty.js. CV

---
+++ src/challenges/04-Arrays-isEmptyOrNot/is-empty.js
@@ -0,0 +1,7 @@
+/*Create a function called isEmpty(arr) that receives an array and returns true if it is empty, or false if it contains elements.
+Example input:
+([])
+Expected output:
+true*/
+
+export const isEmpty = (arr) => arr.length === 0;
