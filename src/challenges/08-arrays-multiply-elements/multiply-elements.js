90e55daee273910eafef55133fbd33e080da0de5
cvega <carlosjuniorrvega@hotmail.com>
2025-11-12 17:43:03 -0500

    feat(08-arrays-multiply-elements): solve challenge multiply-elements.js. CV

---
+++ src/challenges/08-arrays-multiply-elements/multiply-elements.js
@@ -0,0 +1,6 @@
+/*
+Create a function called multiplyAll(arr, multiplier) that takes an array of numbers and a multiplier, then returns a new array where each element is multiplied by the multiplier.
+Example:
+multiplyAll([1, 2, 3], 3)
+*/
+export const multiplyAll=(arr,multiplier)=>arr.map(el=>el*multiplier)
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { isEmpty } from "./challenges/04-Arrays-isEmptyOrNot/is-empty.js";
 import { removeDuplicates } from "./challenges/05-Arrays-remove-duplicates/remove-duplicates.js";
 import { sumEvenNumbers } from "./challenges/06-arrays-sum-even-numbers/sum-even-numbers.js";
 import { getOddNumbers } from "./challenges/07-arrays-odd-numbers/odd-numbers.js";
+import { multiplyAll } from "./challenges/08-arrays-multiply-elements/multiply-elements.js";
 console.log(containElement([3,3,4,5,6],6));
 console.log(findLargestNumber([1,3,10,5,6,7]))
 console.log(countOccurrences([1,2,2,3,2],2))
@@ -14,3 +15,4 @@ console.log(isEmpty([]))
 console.log(removeDuplicates([1,2,2,3,3,4]))
 console.log(sumEvenNumbers([2,2,3]))
 console.log(getOddNumbers([1, 2, 3, 4, 5, 6]))
+console.log(multiplyAll([1,2,3],3))
