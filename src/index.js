e61212dbddd3d5885ee262a9dc7c8f64ae16b638
cvega <carlosjuniorrvega@hotmail.com>
2025-11-13 20:32:51 -0500

    chore(11-arrays-filter-positive-numbers): correct name of folder. CV

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { getOddNumbers } from "./challenges/07-arrays-odd-numbers/odd-numbers.js
 import { multiplyAll } from "./challenges/08-arrays-multiply-elements/multiply-elements.js";
 import { getStringLengths } from "./challenges/09-arrays-string-lengths/get-string-lengths.js";
 import { findIndexOfvalue } from "./challenges/10-array-index-of-value/find-index-of-value.js";
-import { getPositiveNumbers } from "./challenges/12-arrays-filter-positive-numbers/get-positive-numbers.js";
+import { getPositiveNumbers } from "./challenges/11-arrays-filter-positive-numbers/get-positive-numbers.js";
 console.log(containElement([3,3,4,5,6],6));
 console.log(findLargestNumber([1,3,10,5,6,7]))
 console.log(countOccurrences([1,2,2,3,2],2))
