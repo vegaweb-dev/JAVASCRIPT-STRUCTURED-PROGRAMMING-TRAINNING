b4ae151ef36414d58abaf6d462faa9b5d6f6c2b0
cvega <carlosjuniorrvega@hotmail.com>
2025-11-21 17:21:04 -0500

    chore(14-strings-count-vowels): add description. CV

--- src/challenges/14-strings-count-vowels/count-vowels.js
+++ src/challenges/14-strings-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
+/*Exercise 17 — “Count vowels”
+
+Create a function countVowels(str) that returns how many vowels (a, e, i, o, u, case-insensitive) are present in the given string.
+
+Example
+countVowels("Hello World")  // 3
+countVowels("AEIOU")        // 5
+countVowels("xyz")          // 0 */
+
 export const countVowels = (str) =>
   str
     .toLowerCase()
