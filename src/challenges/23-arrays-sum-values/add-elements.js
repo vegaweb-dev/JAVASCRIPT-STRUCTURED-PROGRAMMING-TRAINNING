b4e1dcdfa8b3d0bc2ce328deb961daf29dcf6fd3
cvega <carlosjuniorrvega@hotmail.com>
2025-12-20 13:53:50 -0500

    refactor(23-arrays-sum-values): adjust logic to statement. CV

--- src/challenges/23-arrays-sum-values/add-elements.js
+++ src/challenges/23-arrays-sum-values/add-elements.js
@@ -1,5 +1,12 @@
 // Create a function sumDigits(n) that receives a non-negative integer and returns the sum of all its digits.
-export const sumDigits = (number) =>
-	number > 0
-		? number.split("").map(el=>+el).reduce((p, c) => p + c)
-		: `It only accepts possitive numbers`;
+export const sumDigits = (number) => {
+	if (number < 0) {
+		throw new Error("It only accepts possitive numbers");
+	}
+
+	return number
+		.toString()
+        .split("")
+        .map(el=>+el)
+		.reduce((p, c) => p + c, 0);
+};
